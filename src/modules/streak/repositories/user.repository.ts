d20747565e6fbe8df0d7573dac405e39139e07cb
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-19 21:54:46 -0300

    feat: add user repository

---
+++ src/modules/streak/repositories/user.repository.ts
@@ -0,0 +1,56 @@
+import { Pool } from 'pg';
+import { db } from 'src/config/db';
+import { User } from 'src/modules/streak/entities/User';
+import { Post } from '../entities/Post';
+import { Read } from '../entities/Read';
+
+export class UserRepository {
+  constructor(private readonly database: Pool) {}
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    const result = await this.database.query(
+      `SELECT * FROM users WHERE email = $1;`,
+      [email],
+    );
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    const row = result.rows[0];
+    return new User(
+      row.id,
+      row.email,
+      row.recordStreak,
+      row.createdAt,
+      row.updatedAt,
+    );
+  }
+
+  async createUser(email: string): Promise<User | null> {
+    const result = await this.database.query(
+      `INSERT INTO users ("email") 
+        VALUES ($1) RETURNING *;`,
+      [email],
+    );
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    const row = result.rows[0];
+    return new User(
+      row.id,
+      row.email,
+      row.recordStreak,
+      row.createdAt,
+      row.updatedAt,
+    );
+  }
+}
+
+export const makeUserRepository = () => {
+  const dbPool = db;
+
+  return new UserRepository(dbPool);
+};
