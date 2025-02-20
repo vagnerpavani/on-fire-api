6cd55b657ff4742118d95c7ea1250f99c2128a50
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-19 21:56:13 -0300

    feat: add register daily read use case

---
+++ src/modules/streak/use-cases/register-daily-read/use-case.ts
@@ -0,0 +1,59 @@
+import { RegisterReadDto } from '../../dtos/add-streak.dto';
+import {
+  makePostRepository,
+  makeStreakRepository,
+  makeUserRepository,
+  PostRepository,
+  StreakRepository,
+  UserRepository,
+} from '../../repositories';
+import * as dayjs from 'dayjs';
+
+export class RegisterDailyReadUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly postRepository: PostRepository,
+    private readonly streakRepository: StreakRepository,
+  ) {}
+
+  async execute(read: RegisterReadDto) {
+    const userExists = await this.userRepository.getUserByEmail(read.email);
+    const postExists = await this.postRepository.getPostByBeehivId(read.postId);
+
+    const user = userExists
+      ? userExists
+      : await this.userRepository.createUser(read.email);
+
+    const post = postExists
+      ? postExists
+      : await this.postRepository.createPost(
+          read.postId,
+          read.title,
+          read.publishedAt,
+        );
+
+    const readAlreadyExists =
+      await this.streakRepository.findReadByPostAndUserId(user.id, post.id);
+
+    if (readAlreadyExists) return;
+
+    return await this.streakRepository.createRead(user.id, post.id, {
+      source: read.utmSource,
+      medium: read.utmMedium,
+      campaign: read.utmCampaign,
+      channel: read.utmChannel || null,
+    });
+  }
+}
+
+export const makeRegisterDailyReadUseCase = () => {
+  const userRepository = makeUserRepository();
+  const postRepository = makePostRepository();
+  const streakRepository = makeStreakRepository();
+
+  return new RegisterDailyReadUseCase(
+    userRepository,
+    postRepository,
+    streakRepository,
+  );
+};
