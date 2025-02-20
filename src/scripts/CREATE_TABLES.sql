fe999e112c25f623cdad5d0d6cbaedc63b50a219
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-19 21:54:27 -0300

    chore: add SQL script the database tables

---
+++ src/scripts/CREATE_TABLES.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	email TEXT UNIQUE,
+	"recordStreak" INTEGER DEFAULT 0,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"updatedAt" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	"beehivId" TEXT,
+	title TEXT,
+	"publishedAt" TIMESTAMP NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"updatedAt" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "reads" (
+	id SERIAL PRIMARY KEY,
+	"userId" INTEGER NOT NULL REFERENCES users(id),
+	"postId" INTEGER NOT NULL REFERENCES posts(id),
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"utmSource" TEXT,
+	"utmMedium" TEXT,
+	"utmCampaign" TEXT,
+	"utmChannel" TEXT
+);
+
+CREATE INDEX "idxEmail"
+ON users (email);
+
+CREATE INDEX "idxBeehivId"
+ON posts("beehivId");
