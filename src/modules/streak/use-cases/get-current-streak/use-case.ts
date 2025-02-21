e2b1892e51431a35064e1d6317c93333e71d1e44
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-21 16:53:25 -0300

    feat: add get current streak use case

---
+++ src/modules/streak/use-cases/get-current-streak/use-case.ts
@@ -0,0 +1,60 @@
+import {
+  makeStreakRepository,
+  makeUserRepository,
+  StreakRepository,
+  UserRepository,
+} from '../../repositories';
+import * as dayjs from 'dayjs';
+import { UserNotFoundException } from '../protocols';
+import { WEEKDAYS } from '../constants';
+
+export class GetCurrentStreakUseCase {
+  constructor(
+    private readonly streakRepository: StreakRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async execute(userId: string) {
+    const userWithReads =
+      await this.streakRepository.getReadsFromUserOrderByCreationDesc(userId);
+    if (!userWithReads) throw new UserNotFoundException();
+
+    const user = userWithReads.user;
+    const reads = userWithReads.reads;
+    let currentStreak = 1;
+    let index = 0;
+
+    do {
+      if (index + 1 >= reads.length) break;
+
+      const currentReadDate = reads[index].createdAt;
+      const isMonday = dayjs(currentReadDate).day() === WEEKDAYS.MONDAY;
+      const expectedPrevDate = isMonday
+        ? dayjs(currentReadDate).subtract(2, 'day')
+        : dayjs(currentReadDate).subtract(1, 'day');
+      const prevDayReadDate = reads[index + 1].createdAt;
+
+      if (
+        dayjs(prevDayReadDate).format('DD/MM/YYYY') !==
+        dayjs(expectedPrevDate).format('DD/MM/YYYY')
+      )
+        break;
+
+      currentStreak++;
+      index++;
+    } while (index < reads.length);
+
+    if (currentStreak <= user.recordStreak) return { currentStreak, user };
+
+    user.recordStreak = currentStreak;
+    await this.userRepository.updateUser(user);
+    return { currentStreak, user };
+  }
+}
+
+export const makeGetCurrentStreakUseCase = () => {
+  const streakRepository = makeStreakRepository();
+  const userRepository = makeUserRepository();
+
+  return new GetCurrentStreakUseCase(streakRepository, userRepository);
+};
--- src/modules/streak/use-cases/index.ts
+++ src/modules/streak/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from './register-daily-read';
+export * from './get-current-streak';
 export * from './protocols';
