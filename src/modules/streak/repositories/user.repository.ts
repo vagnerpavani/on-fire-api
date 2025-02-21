4df057357ef3b161130bdcd64272ee4eba4c95dd
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-21 18:31:51 -0300

    feat: add get user by Id

--- src/modules/streak/repositories/user.repository.ts
+++ src/modules/streak/repositories/user.repository.ts
@@ -26,6 +26,26 @@ export class UserRepository {
     );
   }
 
+  async getUserById(userId: string): Promise<User | null> {
+    const result = await this.database.query(
+      `SELECT * FROM users WHERE id = $1;`,
+      [userId],
+    );
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    const row = result.rows[0];
+    return new User(
+      row.id,
+      row.email,
+      row.recordStreak,
+      row.createdAt,
+      row.updatedAt,
+    );
+  }
+
   async createUser(email: string): Promise<User | null> {
     const result = await this.database.query(
       `INSERT INTO users ("email") 
